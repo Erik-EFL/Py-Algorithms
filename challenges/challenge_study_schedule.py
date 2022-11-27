c446a20a5277571332db542cf8f5a3f9820e5eba
erik-efl <erik.lima.f@outlook.com>
2022-11-27 15:09:43 -0300

    feat: :sparkles: Implementing a new function
    implement a recursive function

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if type(target_time) != int:
+        return None
+
+    students  = 0
+
+    for entry, exit in permanence_period:
+        if type(entry) != int or type(exit) != int:
+            return None
+
+        if entry <= target_time <= exit:
+            students += 1
+
+    return students
