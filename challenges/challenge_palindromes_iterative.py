c96dc9829e63a05132b087ff9b1a17ae315ac5e8
erik-efl <erik.lima.f@outlook.com>
2022-11-27 19:21:18 -0300

    feat: :sparkles: Implements a new function
    Crea a function for analize is a palindrome

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,16 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == "":
+        return False
+
+    left_index = 0
+    right_index = len(word) - 1
+
+    while left_index < right_index:
+        if word[left_index] != word[right_index]:
+            return False
+
+        left_index += 1
+        right_index -= 1
+
+    return True
+
