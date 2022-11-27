8ff55ecee8e94b0e7ca9b7d01377dcf7b18c89e3
erik-efl <erik.lima.f@outlook.com>
2022-11-27 17:46:45 -0300

    feat: :sparkles: Implements anew function

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,9 +1,13 @@
 def is_anagram(first_string, second_string):
+    """Returns True if the two strings are anagrams of each other, False otherwise. But don't use the sorted function!"""
+    first_string = first_string.lower()
+    second_string = second_string.lower()
     if len(first_string) != len(second_string):
         return False
-    if len(first_string) == 0:
-        return False
-    for char in first_string:
-        if char not in second_string:
-            return False
-    return True
+    else:
+        for letter in first_string:
+            if letter in second_string:
+                second_string = second_string.replace(letter, "", 1)
+            else:
+                return False
+        return True
