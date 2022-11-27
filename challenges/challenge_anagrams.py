73174df40dedfb672701be45dd9d0a985615b84d
erik-efl <erik.lima.f@outlook.com>
2022-11-27 17:49:13 -0300

    fix: :bug: fix lint error

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,5 +1,4 @@
 def is_anagram(first_string, second_string):
-    """Returns True if the two strings are anagrams of each other, False otherwise. But don't use the sorted function!"""
     first_string = first_string.lower()
     second_string = second_string.lower()
     if len(first_string) != len(second_string):
