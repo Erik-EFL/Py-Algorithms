9f711eba483fe22b97a716bea5062efeac0bf33a
erik-efl <erik.lima.f@outlook.com>
2022-11-27 20:58:25 -0300

    feat: :sparkles: Adding a new function
    Creat a functoion for find diplicate numbers

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,14 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(nums, list) or not all(
+        isinstance(num, int) for num in nums
+    ):
+        return False
+
+    nums.sort()
+
+    for index, num in enumerate(nums[:-1]):
+        if num == nums[index + 1] and num >= 0:
+            return num
+
+    return False
+
