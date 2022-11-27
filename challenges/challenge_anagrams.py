7ed046994122fb8faea33cea3c7acff95208ebd8
erik-efl <erik.lima.f@outlook.com>
2022-11-27 19:10:03 -0300

    refactor: :sparkles: Refact code for pass in all testes

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,12 +1,28 @@
+def merge_sort(array):
+    if len(array) <= 1:
+        return "".join(array)
+
+    middle = len(array) // 2
+    left, right = merge_sort(array[:middle]), merge_sort(array[middle:])
+    return "".join(merge(left, right, array.copy()))
+
+def merge(left, right, merged):
+    left_cursor, right_cursor = 0, 0
+    while left_cursor < len(left) and right_cursor < len(right):
+        if left[left_cursor] < right[right_cursor]:
+            merged[left_cursor + right_cursor] = left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + right_cursor] = right[right_cursor]
+            right_cursor += 1
+
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + right_cursor] = left[left_cursor]
+
+    for right_cursor in range(right_cursor, len(right)):
+        merged[left_cursor + right_cursor] = right[right_cursor]
+
+    return merged
+
 def is_anagram(first_string, second_string):
-    first_string = first_string.lower()
-    second_string = second_string.lower()
-    if len(first_string) != len(second_string):
-        return False
-    else:
-        for letter in first_string:
-            if letter in second_string:
-                second_string = second_string.replace(letter, "", 1)
-            else:
-                return False
-        return True
+    return merge_sort(list(first_string)) == merge_sort(list(second_string))
