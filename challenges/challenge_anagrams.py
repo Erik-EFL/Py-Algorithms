035a00c3011d81c22b2bfee5ca024398edafc132
erik-efl <erik.lima.f@outlook.com>
2022-11-27 17:36:56 -0300

    feat: :sparkles: Implements a new function
    create anagran identify function

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,9 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(first_string) != len(second_string):
+        return False
+    if len(first_string) == 0:
+        return False
+    for char in first_string:
+        if char not in second_string:
+            return False
+    return True
