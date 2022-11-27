35f3f1dbcea1a3646fae151759126eae1e5ad8b2
erik-efl <erik.lima.f@outlook.com>
2022-11-27 17:38:15 -0300

    fix: :bug: Fix lint bugs

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,7 +2,7 @@ def study_schedule(permanence_period, target_time):
     if type(target_time) != int:
         return None
 
-    students  = 0
+    students = 0
 
     for entry, exit in permanence_period:
         if type(entry) != int or type(exit) != int:
